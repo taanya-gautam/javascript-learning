9517d53843792cc90209f55a0d8ccc30f72e600c
taanyagautam <taanya.gaautam@gmail.com>
2022-02-09 13:43:24 +0530

    js basic

---
+++ index.js
@@ -0,0 +1,55 @@
+/*var a;
+a=10;
+console.log(a);
+a="hello";
+console.log(a);
+a=true;
+console.log(a);
+console.log(typeof a)
+a=null;
+console.log(a);
+console.log(typeof a);
+a=10;
+console.log(a);
+*/console.log(typeof a)
+var num=10;
+console.log(num);
+var float=2.4;
+console.log(float);
+var t=true;
+var f=false;
+console.log(t);
+console.log(f);
+var str='a'
+console.log(typeof str);
+str="how are you?"
+console.log(str);
+str="how you doing\n i am Fine.how about you?"
+console.log(str);
+var b="hi hope you guys are able to grasp the concepts";
+console.log(b);
+var num=4500;
+console.log(`halfof ${num}  is ${num/2}`);
+var r="hello";
+console.log(r);
+var r=100;
+console.log(r)
+//let l=100;
+//console.log(l);
+
+//let l="200";
+//console.log(l);
+
+let l=200;
+console.log(l);
+l=300;
+console.log(l)
+
+var num=10;
+for(var i=0;i<=num;i++){
+    if(i%2==10){
+        console.log("num is even");
+    }
+}
+
+
