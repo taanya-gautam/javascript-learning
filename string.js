4fb2612d49e1584f92cc54d41c7fcaf5c92b64d6
taanyagautam <taanya.gaautam@gmail.com>
2022-02-14 00:07:34 +0530

    string

---
+++ string.js
@@ -0,0 +1,27 @@
+var str="hello pepcoders";
+console.log(str);
+//console.log(str[4]);
+//let slicedStr=str.slice(2,7);
+//console.log(slicedStr);
+//console.log(str);
+//var slicedStr=str.slice(6)
+//var slicedStr =str.slice(2,-4);
+//console.log(slicedStr);
+//let ans=str.substr(2,6);
+//console.log(ans);
+//console.log(str);
+//str ="YOLO";
+//console.log(str);
+//console.log(str.toLowerCase());
+//str="fomo"
+//console.log(str.toUpperCase());
+let firstStr= "believe";
+let secondStr="in yourself"
+let concatenatedStr=firstStr+secondStr;
+console.log(concatenatedStr);
+let concatStr=firstStr.concat(secondStr, " and  me");
+console.log(concatStr);
+let trimStr="          hello   ";
+console.log(trimStr);
+console.log(trimStr.trim());
+
